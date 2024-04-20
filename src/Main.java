6aace78777b0f1d6dcce889a8cc7a1cc48ad1238
cyberpropper <osankin2002@mail.ru>
2024-04-20 22:51:06 +0700

    hw-2(2and3lvl)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
+        var dog = (8.0 + 4) - 3.5;
         System.out.println(dog);
-        var cat = 3.6;
+        var cat = (3.6 + 4) - 1.6;
         System.out.println(cat);
-        var paper = 763789;
+        var paper = (763789 + 4) - 7639;
         System.out.println(paper);
     }
 }
