d68609d29cd1f754913c58e0e581a6005a517b41
cyberpropper <osankin2002@mail.ru>
2024-04-20 23:11:14 +0700

    hw-2(5lvl)

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         //System.out.println(cat);
         //var paper = (763789 + 4) - 7639;
         //System.out.println(paper);
-        //var friend = 19;
-        //System.out.println(friend);
-        //var friend2 = friend + 2;
-        //System.out.println(friend2);
-        //var friend3 = friend2 / 7;
-        //System.out.println(friend3);
+        var friend = 19;
+        System.out.println(friend);
+        var friend2 = friend + 2;
+        System.out.println(friend2);
+        var friend3 = friend2 / 7;
+        System.out.println(friend3);
         var frog = 3.5;
         System.out.println(frog);
         var frog2 = frog * 10;
