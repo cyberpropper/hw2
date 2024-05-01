f89fb52ce53747038c3a831fd3786aabcdd3b9ef
cyberpropper <osankin2002@mail.ru>
2024-05-01 17:15:17 +0700

    hw-2(8(1,2)lvl)

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         var frog = 3.5;
-        System.out.println("жаба1" + frog);
+        System.out.println("жаба1 " + frog);
         var frog2 = frog * 10;
-        System.out.println("жаба2" + frog2);
+        System.out.println("жаба2 " + frog2);
         var frog3 = frog2 / 3.5;
-        System.out.println("жаба3" + frog3);
+        System.out.println("жаба3 " + frog3);
         var frog4 = frog3 + 4;
-        System.out.println("жаба4" + frog4);
+        System.out.println("жаба4 " + frog4);
 
         var boxer1 = 78.2;
         var boxer2 = 82.7;
         var allBoxer = boxer1 + boxer2;
-        System.out.println("общий вес" + allBoxer );
+        System.out.println("общий вес " + allBoxer );
         var diff = boxer2 - boxer1;
-        System.out.println("разница" + diff);
+        System.out.println("разница " + diff);
         var rem = boxer2 % boxer1;
-        System.out.println("остаток от деления" + rem);
+        System.out.println("остаток от деления " + rem);
 
         var wTime = 640;
         var workers = wTime / 8;
