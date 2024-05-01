128f05cd25b93f5ef39650896632dcf749870440
cyberpropper <osankin2002@mail.ru>
2024-05-01 18:55:27 +0700

    hw-2(master)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         var dog = (8.0 + 4) - 3.5;
         System.out.println(dog);
         var cat = (3.6 + 4) - 1.6;
-        System.out.println(cat );
+        System.out.println(cat);
         var paper = (763789 + 4) - 7639;
         System.out.println(paper);
         var friend = 19;
